a20aaa2d12a77fb50338c6f85a800db7ca58f63e
Joseph Mikko Mañoza <jm.taisondigital@gmail.com>
2020-02-05 11:23:35 +0800

    Initial Commit

---
+++ Fox Weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fox Weather
+//
+//  Created by Joseph Mikko Mañoza on 05/02/2020.
+//  Copyright © 2020 Joseph Mikko Mañoza. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
